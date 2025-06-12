1ebba97ce74ec8a0e104ff853ea7d0fcd8652f06
codextra <codextrafreelancer@gmail.com>
2025-06-12 12:37:24 +0100

    seller add fix

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
     _id: { type: String, required: true },
-    userId: {type: String},
+    userId: {type: String, required: true},
     name: {type: String, required: true},
     category: {type: String, required: true},
     description: {type: String, required: true},
